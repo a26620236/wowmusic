5bebac95fd3bb7a5a4ddcbe195f18ac450ae6c4c
a26620236 <a26620236@gmail.com>
2020-08-10 17:34:38 +0800

    update testing

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 import { timeFormat, MobileBar } from './src/components/musicplayer'
 import React from 'react';
 import renderer from 'react-test-renderer';
-import axios from 'axios'
 
 test("test currentTime format", () => {
   expect(timeFormat(25)).toBe('0:25')
@@ -26,23 +25,3 @@ it('renders correctly', () => {
   expect(tree).toMatchSnapshot();
 });
 
-const API = 'https://jsonplaceholder.typicode.com/posts/1'
-
-const getData = function() {
-  return axios.get(API).then(resp => resp.data)
-}
-
-jest.mock('axios');
-
-test('should fetch post', () => {
-  const users = {
-    "userId": 1,
-    "id": 1,
-    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-  }
-  const resp = { data: users }
-  axios.get.mockResolvedValue(resp);
-
-  return getData().then(data => expect(data).toEqual(users));
-})
